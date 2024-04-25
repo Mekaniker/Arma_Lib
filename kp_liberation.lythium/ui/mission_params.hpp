d06825008857ef3a1a407d493ab2f94548008af4
Mark Goossens <120757600+Jennlehgamer@users.noreply.github.com>
2024-04-25 10:45:01 +0200

    Update mission_params.hpp

--- kp_liberation.lythium/ui/mission_params.hpp
+++ kp_liberation.lythium/ui/mission_params.hpp
@@ -309,7 +309,7 @@ class Params {
         title = $STR_PARAM_ZEUSADDENEMIES;
         values[] = {1, 0};
         texts[] = {$STR_PARAMS_ENABLED, $STR_PARAMS_DISABLED};
-        default = 1;
+        default = 0;
     };
     class HighCommand {
         title = $STR_PARAM_HIGHCOMMAND;
